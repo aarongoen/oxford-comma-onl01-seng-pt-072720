89447e86fbe1b3a21bb4b53c6e6a303bcb1ac0ac
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-05 18:37:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,12 +4,14 @@ def oxford_comma(array)
   elsif array.length == 2 
     puts array.join(" and ")
   else
-#     # array.join(',')
-#     array.join(", ")
-#     array.insert(-2, " and ")
-#   end
-# end
+#   
     new_array = array[0..-2] 
     return new_array.join(", ") + ", and " + array[-1]
   end
 end 
+
+  # array.join(',')
+#     array.join(", ")
+#     array.insert(-2, " and ")
+#   end
+# end
