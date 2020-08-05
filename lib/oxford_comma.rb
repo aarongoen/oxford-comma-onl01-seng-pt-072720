819b57d0c0c41fac34d8e70b747fb3dc5d2471e5
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-05 18:06:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,1 +1,16 @@
-arar
+array = ["fiddleheads", "okra", "kohlrabi"]
+
+def oxford_comma(array)
+  if array.count == 1
+    puts array
+  elsif array.count == 2 
+    puts array.join(" and ")
+  else
+    # new_array = []
+    # array.each do |veggie|
+      array[0..-2].join(", ") + ", and " + array[-1]
+    end
+    
+  # end 
+  # return new_array
+end
