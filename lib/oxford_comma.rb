fe2371c4c37a57008ab00252f3091b6a3c42460e
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-05 18:11:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ array = ["fiddleheads", "okra", "kohlrabi"]
 
 def oxford_comma(array)
   if array.count == 1
-    puts array
+    puts array[0]
   elsif array.count == 2 
-    puts array.join(" and ")
+    puts array[0..1].join(" and ")
   else
     array[0..-2].join(", ") + ", and " + array[-1]
     end
